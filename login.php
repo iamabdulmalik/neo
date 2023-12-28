68f0c9d8c9c6a8ec57cb5820c8c41d9a0e937543
iamabdulmalik <147354729+iamabdulmalik@users.noreply.github.com>
2023-12-27 18:27:33 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <form action="insert.php" method="POST">
+        <table>
+            <tr>
+                <td> Email :</td>
+                <td><input type="text" name="email" required></td>
+            </tr>
+            <tr>
+               <td>password :</td>
+               <td><input type="password" name="password" required></td> 
+            </tr>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Submit" class="form-btn"></td>
+        </tr>
+        </table>
+        Networking empire <a href="form.html">logout</a>
+    </form>
+</body>
+</html>
